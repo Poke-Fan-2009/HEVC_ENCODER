1bd330311e91cedd23befd6c69f30c99b83d356e
Naysa Bots <96517910+Naysabots@users.noreply.github.com>
2022-01-06 20:35:57 +0530

    Create commands.py

---
+++ NaysaEncoderBot/commands.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from bot.get_cfg import get_config
+
+
+class Command:
+    COMPRESS = get_config(
+        "COMMAND_COMPRESS",
+        "compress"
+    )
+    CANCEL = get_config(
+        "COMMAND_CANCEL",
+        "cancel"
+    )
+    STATUS = get_config(
+        "COMMAND_STATUS",
+        "status"
+    )
+    EXEC = get_config(
+        "COMMAND_EXEC",
+        "exec"
+    )
+    HELP = get_config(
+        "COMMAND_HELP",
+        "help"
+    )
+    UPLOAD_LOG_FILE = get_config(
+        "COMMAND_UPLOAD_LOG_FILE",
+        "log"
+    )
+    TEXT = "Send Resolution Like This /set_res 720p"
