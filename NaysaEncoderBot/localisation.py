efb062f64f2b619ca8e2a1120439ccb5469530a9
Naysa Bots <96517910+Naysabots@users.noreply.github.com>
2022-01-06 20:32:44 +0530

    Create localisation.py

---
+++ NaysaEncoderBot/localisation.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) @AbirHasan2005
+
+from bot.get_cfg import get_config
+
+
+class Localisation:
+   
+    ABS_TEXT = " Please don't be selfish."
+    
+    FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
+    
+    
+    DOWNLOAD_START = "📥 Downloading ...\n"
+    
+    UPLOAD_START = "📤 Uploading ... \n"
+    
+    COMPRESS_START = "👀 Trying To Encode..."
+    
+    RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
+    
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations."
+    
+    COMPRESS_SUCCESS = "©️ @MediaEncoderBot"
+
+    COMPRESS_PROGRESS = "⏳ ETA: {}\n🚀 Progress: {}%"
+
+    SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
+    
+    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
+    
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
+    
+    SAVED_RECVD_DOC_FILE = "✅ Downloaded Successfully."
+    
+    CUSTOM_CAPTION_UL_FILE = " "
+    
+    NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
+    
+    NO_VOID_FORMAT_FOUND = "no-one gonna help you\n{}"
+    
+    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
+    
+    FF_MPEG_RO_BOT_STOR_AGE_ALREADY_EXISTS = "⚠️ Already one Process going on! ⚠️ \n\nCheck Live Status on Updates Channel."
+    
+    WRONG_MESSAGE = get_config(
+        "STRINGS_WRONG_MESSAGE",
+        "current CHAT ID: <code>{CHAT_ID}</code>"
+    )
+
+    
