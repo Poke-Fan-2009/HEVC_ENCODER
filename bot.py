0939f9f71c9991b60268dbcaae2381fd2d67d766
Naysa Bots <96517910+Naysabots@users.noreply.github.com>
2022-01-06 21:26:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ if __name__ == "__main__" :
     naysabots = Client(
         "Id Finder Bot",
         bot_token=Config.TG_BOT_TOKEN,
-        api_id=Config.API_ID,
+        api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
         plugins=plugins,
         workers=100
