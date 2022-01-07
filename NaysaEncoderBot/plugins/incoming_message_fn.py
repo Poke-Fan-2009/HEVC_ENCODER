a9fc731a298f7b9c8f3cdce7bf945274f963b05c
Naysa Bots <96517910+Naysabots@users.noreply.github.com>
2022-01-07 17:34:37 +0530

    Update incoming_message_fn.py

--- NaysaEncoderBot/plugins/incoming_message_fn.py
+++ NaysaEncoderBot/plugins/incoming_message_fn.py
@@ -27,8 +27,6 @@ from NaysaEncoderBot.helper_funcs.display_progress import (
   humanbytes
 )
 from pyrogram.types import Message
-from NaysaEncoderBot import AUTH_USERS
-from NaysaEncoderBot.adduser import present_in_userbase, add_to_userbase, get_users
 import time
 from pyrogram import Client, filters
 from pyrogram.handlers import MessageHandler, CallbackQueryHandler
@@ -449,32 +447,4 @@ async def incoming_cancel_message_f(bot, update):
       text="No active compression exists",
       reply_to_message_id=update.message_id
     )
-@Client.on_message(filters.private & filters.command('broadcast') & filters.user(AUTH_USERS) & filters.reply)
-async def broadcast(client: Client, message: Message):
-       broadcast_msg = message.reply_to_message
-       txt = await message.reply(text = 'Staring....')        
-       user_ids = await get_users()
-       success = 0
-       deleted = 0
-       blocked = 0     
-       await txt.edit(text = 'Broadcasting message, Please wait', reply_markup = None)   
-       for user_id in user_ids:
-          try:
-            broadcast_msg = await broadcast_msg.copy(
-            chat_id =user_id ,
-            reply_to_message_id = broadcast_msg.message_id
-            )
-            success += 1
-            time.sleep(3)
-          except FloodWait as e:
-            await asyncio.sleep(e.x)
-            success += 1
-          except UserIsBlocked:
-            blocked += 1
-          except InputUserDeactivated:
-            deleted += 1                       
-       text = f"""<b>Broadcast Completed</b>    
-Total users: {str(len(user_ids))}
-Deleted accounts: {str(deleted)} """
-       await message.reply(text=text)
-       await message.delete()
+
