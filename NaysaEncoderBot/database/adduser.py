d6242ea074d3aa1b3e2b970c50432b470d316bc7
Naysa Bots <96517910+Naysabots@users.noreply.github.com>
2022-01-07 13:28:14 +0530

    Update adduser.py

--- NaysaEncoderBot/database/adduser.py
+++ NaysaEncoderBot/database/adduser.py
@@ -8,25 +8,25 @@ database = dbclient["Name"]
 
 user_collection = database['users']
 
-async def present_in_userbase(user_id : int):
-    found = user_collection.find_one({'_id': user_id})
-    if found:
-        return True
-    else:
-        return False
+    async def present_in_userbase(user_id : int):
+        found = user_collection.find_one({'_id': user_id})
+        if found:
+            return True
+        else:
+            return False
 
-async def add_to_userbase(user_id: int):
-    user_collection.insert_one({'_id': user_id})
-    return
+    async def add_to_userbase(user_id: int):
+        user_collection.insert_one({'_id': user_id})
+        return
 
-async def get_users():
-    user_docs = user_collection.find()
-    user_ids = []
-    for doc in user_docs:
-        user_ids.append(doc['_id'])
+    async def get_users():
+        user_docs = user_collection.find()
+        user_ids = []
+        for doc in user_docs:
+            user_ids.append(doc['_id'])
         
-    return user_ids
+        return user_ids
     
-async def del_from_userbase(user_id: int):
-    user_collection.delete_one({'_id': user_id})
-    return
+    async def del_from_userbase(user_id: int):
+        user_collection.delete_one({'_id': user_id})
+        return
