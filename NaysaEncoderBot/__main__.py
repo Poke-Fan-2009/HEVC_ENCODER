947b3896e928840b1aefc575c027d80e4777d51e
Naysa Bots <96517910+Naysabots@users.noreply.github.com>
2022-01-07 14:52:26 +0530

    Update __main__.py

--- NaysaEncoderBot/__main__.py
+++ NaysaEncoderBot/__main__.py
@@ -155,35 +155,7 @@ if __name__ == "__main__" :
       p = f"🌋Pɪɴɢ = {ms}ms"
       await message.reply_text(v + "\n" + p)
 
-    @app.on_message(filters.private & filters.command('broadcast') & filters.user(AUTH_USERS) & filters.reply)
-    async def broadcast(client: app, message: Message):
-           broadcast_msg = message.reply_to_message
-           txt = await message.reply(text = 'Staring....')        
-           user_ids = await get_users()
-           success = 0
-           deleted = 0
-           blocked = 0     
-           await txt.edit(text = 'Broadcasting message, Please wait', reply_markup = None)   
-           for user_id in user_ids:
-              try:
-                broadcast_msg = await broadcast_msg.copy(
-                chat_id = user_id,
-                reply_to_message_id = broadcast_msg.message_id
-                )
-                success += 1
-                time.sleep(3)
-              except FloodWait as e:
-                await asyncio.sleep(e.x)
-                success += 1
-              except UserIsBlocked:
-                blocked += 1
-              except InputUserDeactivated:
-                deleted += 1                       
-           text = f"""<b>Broadcast Completed</b>    
-    Total users: {str(len(user_ids))}
-    Deleted accounts: {str(deleted)} """
-           await message.reply(text=text)
-           await message.delete()
+
  
              
 
