8cf32a0342144a4f21c48a828a2ca714ff3e40cf
Naysa Bots <96517910+Naysabots@users.noreply.github.com>
2022-01-07 12:50:58 +0530

    Update __main__.py

--- NaysaEncoderBot/__main__.py
+++ NaysaEncoderBot/__main__.py
@@ -111,7 +111,7 @@ if __name__ == "__main__" :
         if TRACE_CHANNEL:
             try:
                 media = await message.copy(chat_id=TRACE_CHANNEL)
-                trace_msg = await media.reply_text(f'User Name:{message.from_user.mention}\n\nUser Id:<code>{message.from_user.id}</code>\n\nStatus: Compressing....')
+                trace_msg = await media.reply_text(f'👀 User Name : {message.from_user.mention}\n\n🚴‍♂️ User Id : <code>{message.from_user.id}</code>\n\n🚦 Status : Added To My Database For Compressing 🗜️...')
             except PeerIdInvalid:
                 logger.warning("Give the correct Channel or Group ID.")
             except ChannelInvalid:
