4f5066b0c229cda57b9fbdf3147615c0ec3cc70a
Naysa Bots <96517910+Naysabots@users.noreply.github.com>
2022-01-07 13:07:29 +0530

    Update database.py

--- NaysaEncoderBot/database/database.py
+++ NaysaEncoderBot/database/database.py
@@ -1,1 +1,38 @@
+from pyrogram.types import Message
+from pyrogram import Client as Tellybots
+from pyrogram import filters
+from NaysaEncoderBot import *
+from NaysaEncoderBot.database.adduser import present_in_userbase, add_to_userbase, get_users # userbase.py is Attached below
+import time
+
+@Tellybots.on_message(filters.private & filters.command('broadcast') & filters.user(OWNER_ID) & filters.reply)
+async def broadcast(client: Tellybots, message: Message):
+       broadcast_msg = message.reply_to_message
+       txt = await message.reply(text = 'Staring....')        
+       user_ids = await get_users()
+       success = 0
+       deleted = 0
+       blocked = 0     
+       await txt.edit(text = 'Broadcasting message, Please wait', reply_markup = None)   
+       for user_id in user_ids:
+          try:
+            broadcast_msg = await broadcast_msg.copy(
+            chat_id =user_id ,
+            reply_to_message_id = broadcast_msg.message_id
+            )
+            success += 1
+            time.sleep(3)
+          except FloodWait as e:
+            await asyncio.sleep(e.x)
+            success += 1
+          except UserIsBlocked:
+            blocked += 1
+          except InputUserDeactivated:
+            deleted += 1                       
+       text = f"""<b>Broadcast Completed</b>    
+Total users: {str(len(user_ids))}
+Deleted accounts: {str(deleted)} """
+       await message.reply(text=text)
+       await message.delete()
+ 
 
